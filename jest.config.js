f98ea9ee715a912acf45a0e113e4b5d517372b8a
Serhii Pimenov <serhii@pimenov.com.ua>
2022-06-16 11:26:57 +0300

    initial

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+export default {
+  verbose: true,
+  collectCoverage: false,
+  "collectCoverageFrom": [
+    "**/src/plugins/*.js",
+    "**/src/core/*.js",
+    "**/src/helpers/*.js",
+    "**/src/dataset/*.js",
+  ]
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Aptos Rest API
