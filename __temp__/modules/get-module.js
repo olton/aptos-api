0be50320ea60f42142d87e5ae8b255e7253e2039
Serhii Pimenov <serhii@pimenov.com.ua>
2022-06-19 11:55:53 +0300

    initial

---
+++ __temp__/modules/get-module.js
@@ -0,0 +1,11 @@
+import {Aptos} from "../../src/index.js";
+import {TEST_ACCOUNT} from "../../__tests__/helpers/address.js";
+import {debug} from "../../src/helpers/debug.js";
+
+const NODE_URL = process.env.APTOS_NODE_URL || 'https://fullnode.devnet.aptoslabs.com';
+
+const api = new Aptos(NODE_URL, {
+    max_gas_amount: 2000
+})
+
+debug(await api.getAccountModules(TEST_ACCOUNT.address))
